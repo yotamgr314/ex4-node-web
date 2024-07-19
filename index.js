4e7d4876b4d62583318e6c7103b5ee6ca2b25ea1
Yotam Green <yotamgr314@gmail.com>
2024-07-19 17:57:06 +0300

    setting up basic express app layout, and packges

---
+++ index.js
@@ -0,0 +1,4 @@
+/* building an express based app */
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
---
+++ routers/userPreferences.js
@@ -0,0 +1,4 @@
+/*
+ a router is essensily a way to create another mini instance of our application that has its own logic, routes, and middlewares.
+ where we can define all the routes that are related to a specific part of our application and later on we can connect it to our main application.
+  */
